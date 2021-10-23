172b4664d8d8a2702d6e79672802e56c163999b4
Danilo Nascimento <ndanilo@hotmail.com>
2021-10-23 13:56:04 +0100

    update

--- lib/Telemetry/telemetry.cpp
+++ lib/Telemetry/telemetry.cpp
@@ -1,10 +1,55 @@
 #include <telemetry.h>
 
-Chrono telemetryimer;
+/* 
+Special Thanks to Boris Du Reau for inspiration on the telemetry packets
+Check his github here: https://github.com/bdureau
+*/
 
-bool Telemetry::sendTelemetry(){
-    // TODO find the correct size
-    char packet[150];
-    char buffer[10];
+Chrono timer;
 
+/*
+Send Telemetry to SD Card
+
+ */
+bool Telemetry::sendTelemetry()
+{
+    if (timer.hasPassed(DATA_SAMPLE_RATE))
+    {
+        // TODO find the correct size
+        char packet[150];
+        char buffer[15];
+        strcat(packet, "telemetry,");
+        sprintf(buffer, "%i,", data.state);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.loopTime);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.altimeter.altitude);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.altimeter.temperature);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.altimeter.verticalVelocity);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.imu.accel.x);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.imu.accel.y);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.imu.accel.z);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.imu.eulerAngles.yaw);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.imu.eulerAngles.pitch);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.imu.eulerAngles.roll);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.batteryVoltage);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.pyro1Continuity);
+        strcat(packet, buffer);
+        sprintf(buffer, "%i,", data.imu.eulerAngles.pitch);
+
+//TODO add checksum? https://github.com/bdureau/RocketFlightLogger/blob/140cf36829486526b69e4608e735478b8145a25b/config.cpp#L377
+        strcat(packet, ";\n");
+        Serial.print("$");
+        Serial.print(packet);
+    }
 }
